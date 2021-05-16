8a3ade687d1b22359296cdb9e5a1dfb41c22e719
mwirth3 <mawirth3@gmail.com>
2021-05-16 10:27:42 -0700

    Updated weather icon to reflect current city

--- src/index.js
+++ src/index.js
@@ -49,7 +49,7 @@ function showTemperature(response) {
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
   currentIconElement.setAttribute(
     "src",
-    `http://openweathermap.org/img/wn/02d@2x.png`
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   let h1 = document.querySelector("h1");
   h1.innerHTML = response.data.name;
